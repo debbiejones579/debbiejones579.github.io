39b8ca85da55ad2b00b94839b330d02c8e22844d
debbiejones579 <debbiejones579@aol.com>
2021-02-15 14:07:35 -0700

    rain

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,14 @@
-function select() {
+function select() { //finding shoes to fit the weather
     var weather = document.getElementById("shoes").value;
 //user input of shoes to equal output of shoes
     switch (weather) {
       case "hot":
-        document.getElementById("out").innerHTML = "Sandals";
+        document.getElementById("out").innerHTML = "Sandals"; // if user enteres hot output sandals
         break;
-      case "rain":
+      case "rain"://if user inouts rain output galoshes
         document.getElementById("out").innerHTML = "Galoshes";
         break;
-      case "cold":
+      case "cold": // if user inouts cold output boots
         document.getElementById("out").innerHTML = "Boots";
         break;
     } }
